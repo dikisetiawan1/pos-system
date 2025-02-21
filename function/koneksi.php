d7e6c31a5b09cf91cde2c39c350cab0dbd342913
Diki Setiawan <dikisetia101@gmail.com>
2025-02-21 09:14:36 +0700

    push all source

---
+++ function/koneksi.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "pos";
+
+$koneksi = mysqli_connect($host, $username, $password, $database);
+
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+} else {
+    echo " ";
+}
+
+
+
+?>
